189093fd3142c8b953a1fc8379d78e29beb2f654
Ashish <juyal.ashish@gmail.com>
2021-08-02 08:39:53 +0530

    added the mysql database file for unit testing workshop

---
+++ docker/02-user.sql
@@ -0,0 +1,4 @@
+USE mysql;
+INSERT INTO `user` VALUES ('%','banking','*EF7ED82FE628D88B78180749486F0B0322C372D3','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','','','','',0,0,0,0,'mysql_native_password','','N');
+FLUSH PRIVILEGES;
+
